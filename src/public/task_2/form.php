96e2cfa653c25f099dea44346458fd57e50b043e
Nikolay <terleew2013@yandex.ru>
2021-11-29 06:33:59 +0000

    feat:create form for task_2

---
+++ src/public/task_2/form.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3" >
+            <!-- form start -->
+            <form >
+                <div class="mb-3 form-group">
+                    <label for="login" class="form-label">Login</label>
+                    <input type="text" name="login" class="form-control" id="login">
+                </div>
+                <div class="mb-3 form-group">
+                    <label for="name" class="form-label">First name</label>
+                    <input type="text" name="name" class="form-control" id="name">
+                </div>
+                <div class="mb-3 form-group">
+                    <label for="age" class="form-label">Age</label>
+                    <input type="text" name="age" class="form-control" id="age">
+                </div>
+                <div class="mb-3 form-group">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" name="email" class="form-control" id="email">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            <!-- form end -->
+
+        </div>
+    </div>
+</div>
--- src/public/task_2/index.php
+++ src/public/task_2/index.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'nav.php';
+require_once 'form.php';
+
 
 
 
